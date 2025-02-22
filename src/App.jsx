e43104981beebd842b19efe633e2166cd2dc1f23
aviralrabbit1 <aviral.atdrive@gmail.com>
2025-02-22 10:07:50 +0530

    :sparkles: Create Calculator app

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Flags from './mini/Flag.';
 import States from './mini/States';
 import Error from './mini/Error';
 import Fullname from './mini/Fullname';
+import Calculator from './mini/Calculator';
 
 function App() {
 
@@ -21,12 +22,16 @@ function App() {
                     <li>
                         <Link to="/fullname">Fullname</Link>
                     </li>
+                    <li>
+                        <Link to="/calculator">Calculator</Link>
+                    </li>
                 </ul>
             </nav>
               <Routes>
                   <Route path="/" index element={<Flags/>} />
                   <Route path="/states" element={<States />} />
                   <Route path="/fullname" element={<Fullname />} />
+                  <Route path="/calculator" element={<Calculator />} />
                   <Route path="/*" element={<Error />} />
               </Routes>
         </div>
